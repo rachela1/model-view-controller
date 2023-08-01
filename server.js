a4133446a161fb5f6c82098c43a8efc869bb9c10
rachela1 <rachel_kaitlin01@hotmail.com>
2023-07-31 20:45:34 -0500

    added session code to server.js. added connection code in connection.js. added router sandwitch in index.js. created css folder and style.css. started mainhandlebars html page.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -11,9 +11,32 @@ const helpers = require('./utils/helpers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('/'));
+const sess = {
+    secret: process.env.SECRET,
+    cookie: 
+    { maxAge: 24 * 60 * 60 * 1000 },
+    resave: false,
+    saveUnititialized: true,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+};
 
-app.listen(PORT, () => {
-    console.log('Now listening')
-})
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
+
+
